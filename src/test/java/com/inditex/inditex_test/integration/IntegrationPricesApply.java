e0c56a35e3749a3d89029b7ba81526b36167e92f
Kendall Navarro <32465893+kendinho32@users.noreply.github.com>
2025-05-18 16:10:42 -0700

    add test parametrized

--- src/test/java/com/inditex/inditex_test/integration/IntegrationPricesApply.java
+++ src/test/java/com/inditex/inditex_test/integration/IntegrationPricesApply.java
@@ -2,8 +2,10 @@ package com.inditex.inditex_test.integration;
 
 import com.inditex.inditex_test.modules.price.records.PriceRecord;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +13,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import java.time.LocalDateTime;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -26,10 +29,12 @@ public class IntegrationPricesApply {
     @BeforeEach
     public void setUp() {}
 
-    @Test
-    void pricesApply_whenCaseFirstTest() {
+    @ParameterizedTest
+    @MethodSource("dataTest")
+    void pricesApply_casesParametrized(String date, String productId, String brandId, Double priceExpected,
+                                       String startDateExpected, String endDateExpected) {
         webTestClient.get()
-                .uri("/apply_date/2020-06-14T10:00:00/product_id/35455/brand_id/1/price")
+                .uri("/apply_date/"+ date +"/product_id/"+ productId +"/brand_id/"+ brandId +"/price")
                 .exchange()
                 .expectStatus()
                 .isOk()
@@ -39,89 +44,19 @@ public class IntegrationPricesApply {
                     assertThat(price.name()).isEqualTo(ZARA);
                     assertThat(price.brandId()).isEqualTo(1L);
                     assertThat(price.productId()).isEqualTo(35455L);
-                    assertThat(price.price()).isEqualTo(35.5);
-                    assertThat(price.startDate()).isEqualTo(LocalDateTime.parse("2020-06-14T00:00:00"));
-                    assertThat(price.endDate()).isEqualTo(LocalDateTime.parse("2020-12-31T23:59:59"));
+                    assertThat(price.price()).isEqualTo(priceExpected);
+                    assertThat(price.startDate()).isEqualTo(LocalDateTime.parse(startDateExpected));
+                    assertThat(price.endDate()).isEqualTo(LocalDateTime.parse(endDateExpected));
                 });
     }
 
-    @Test
-    void pricesApply_whenCaseSecondTest() {
-        webTestClient.get()
-                .uri("/apply_date/2020-06-14T16:00:00/product_id/35455/brand_id/1/price")
-                .exchange()
-                .expectStatus()
-                .isOk()
-                .expectHeader().contentType(MediaType.APPLICATION_JSON)
-                .expectBody(PriceRecord.class)
-                .value(price -> {
-                    assertThat(price.name()).isEqualTo(ZARA);
-                    assertThat(price.brandId()).isEqualTo(1L);
-                    assertThat(price.priority()).isEqualTo(1L);
-                    assertThat(price.productId()).isEqualTo(35455L);
-                    assertThat(price.price()).isEqualTo(25.45);
-                    assertThat(price.startDate()).isEqualTo(LocalDateTime.parse("2020-06-14T15:00:00"));
-                    assertThat(price.endDate()).isEqualTo(LocalDateTime.parse("2020-06-14T18:30:00"));
-                });
-    }
-
-    @Test
-    void pricesApply_whenCaseThreeTest() {
-        webTestClient.get()
-                .uri("/apply_date/2020-06-14T21:00:00/product_id/35455/brand_id/1/price")
-                .exchange()
-                .expectStatus()
-                .isOk()
-                .expectHeader().contentType(MediaType.APPLICATION_JSON)
-                .expectBody(PriceRecord.class)
-                .value(price -> {
-                    assertThat(price.name()).isEqualTo(ZARA);
-                    assertThat(price.brandId()).isEqualTo(1L);
-                    assertThat(price.priority()).isEqualTo(0L);
-                    assertThat(price.productId()).isEqualTo(35455L);
-                    assertThat(price.price()).isEqualTo(35.5);
-                    assertThat(price.startDate()).isEqualTo(LocalDateTime.parse("2020-06-14T00:00:00"));
-                    assertThat(price.endDate()).isEqualTo(LocalDateTime.parse("2020-12-31T23:59:59"));
-                });
-    }
-
-    @Test
-    void pricesApply_whenCaseFourTest() {
-        webTestClient.get()
-                .uri("/apply_date/2020-06-15T10:00:00/product_id/35455/brand_id/1/price")
-                .exchange()
-                .expectStatus()
-                .isOk()
-                .expectHeader().contentType(MediaType.APPLICATION_JSON)
-                .expectBody(PriceRecord.class)
-                .value(price -> {
-                    assertThat(price.name()).isEqualTo(ZARA);
-                    assertThat(price.brandId()).isEqualTo(1L);
-                    assertThat(price.priority()).isEqualTo(1L);
-                    assertThat(price.productId()).isEqualTo(35455L);
-                    assertThat(price.price()).isEqualTo(30.5);
-                    assertThat(price.startDate()).isEqualTo(LocalDateTime.parse("2020-06-15T00:00:00"));
-                    assertThat(price.endDate()).isEqualTo(LocalDateTime.parse("2020-06-15T11:00:00"));
-                });
-    }
-
-    @Test
-    void pricesApply_whenCaseFiveTest() {
-        webTestClient.get()
-                .uri("/apply_date/2020-06-16T21:00:00/product_id/35455/brand_id/1/price")
-                .exchange()
-                .expectStatus()
-                .isOk()
-                .expectHeader().contentType(MediaType.APPLICATION_JSON)
-                .expectBody(PriceRecord.class)
-                .value(price -> {
-                    assertThat(price.name()).isEqualTo(ZARA);
-                    assertThat(price.brandId()).isEqualTo(1L);
-                    assertThat(price.priority()).isEqualTo(1L);
-                    assertThat(price.productId()).isEqualTo(35455L);
-                    assertThat(price.price()).isEqualTo(38.95);
-                    assertThat(price.startDate()).isEqualTo(LocalDateTime.parse("2020-06-15T16:00:00"));
-                    assertThat(price.endDate()).isEqualTo(LocalDateTime.parse("2020-12-31T23:59:59"));
-                });
+    private static Stream<Arguments> dataTest() {
+        return Stream.of(
+                Arguments.of("2020-06-14T10:00:00", "35455", "1", 35.5, "2020-06-14T00:00:00", "2020-12-31T23:59:59"),
+                Arguments.of("2020-06-14T16:00:00", "35455", "1", 25.45, "2020-06-14T15:00:00", "2020-06-14T18:30:00"),
+                Arguments.of("2020-06-14T21:00:00", "35455", "1", 35.5, "2020-06-14T00:00:00", "2020-12-31T23:59:59"),
+                Arguments.of("2020-06-15T10:00:00", "35455", "1", 30.5, "2020-06-15T00:00:00", "2020-06-15T11:00:00"),
+                Arguments.of("2020-06-16T21:00:00", "35455", "1", 38.95, "2020-06-15T16:00:00", "2020-12-31T23:59:59")
+        );
     }
 }
